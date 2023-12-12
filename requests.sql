0ef2d4ebc7e45459630e88449b4b862092a1f212
PeschykhinLev <91117670+PeschykhinLev@users.noreply.github.com>
2023-12-13 00:07:56 +0200

    Update requests.sql
    change

--- requests.sql
+++ requests.sql
@@ -1,22 +1,22 @@
--- Cередне значення кількості калорій у Big Mac та Egg McMuffin
-
-SELECT AVG(Calories) FROM Nutritional_Information
-WHERE item_id IN (
-    SELECT item_id FROM meal WHERE item_name = 'Big Mac' OR item_name = 'Egg McMuffin'
+-- Значення кількості калорій у стравах з категорії "Сніданки"
+SELECT Calories FROM Nutritional_Information
+WHERE item_id = (
+    SELECT item_id FROM meal_category WHERE item_id =(
+		 SELECT category_id FROM category Where category_name = 'Breakfast'
+	)
 );
 
--- Категорія страви, яка має найбільшу кількість калорій
+-- Категорія страви, які мають від 100 до 500 калорій
 SELECT Category.category_name
 FROM Category
 JOIN Meal_Category ON Category.category_id = Meal_Category.category_id
 JOIN Nutritional_Information ON Meal_Category.item_id = Nutritional_Information.item_id
-WHERE Nutritional_Information.Calories = (
-    SELECT MAX(Calories) FROM Nutritional_Information
-);
+WHERE Nutritional_Information.Calories > 100 AND Nutritional_Information.Calories < 500;
 
--- Назва страви, яка має у собі 2% від денної норми споживання вітаміну С
+
+-- Назва страви, яка має у собі більше 2% від денної норми споживання вітаміну С
 SELECT Meal.item_name
 FROM Meal
 JOIN Daily_Values_on_the_Nutrition ON Meal.item_id = Daily_Values_on_the_Nutrition.item_id
-WHERE Daily_Values_on_the_Nutrition.Vitamin_C_Daily_Percentage = 2;
+WHERE Daily_Values_on_the_Nutrition.Vitamin_C_Daily_Percentage > 2;
 
