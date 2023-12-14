eaa53cd5606adc778cc39e137a66cfad4fa649af
PeschykhinLev <91117670+PeschykhinLev@users.noreply.github.com>
2023-12-14 02:42:11 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -13,10 +13,10 @@ WHERE Nutritional_Information.Calories > 100
 ORDER BY Calories;
 
 -- Назва страви, та впорядковане порівняння (за зростаннія) її кількості калорій та % від денної форми жирів
-SELECT Category.category_name, Nutritional_Information.Calories, Daily_Values_on_the_Nutrition.Total_Fat_Daily_Percentage
-FROM Category
-JOIN Meal_Category ON Category.category_id = Meal_Category.category_id
-JOIN Nutritional_Information ON Meal_Category.item_id = Nutritional_Information.item_id
-JOIN Daily_Values_on_the_Nutrition ON Meal_Category.item_id = Daily_Values_on_the_Nutrition.item_id
+SELECT Meal.item_name, Nutritional_Information.Calories, Daily_Values_on_the_Nutrition.Total_Fat_Daily_Percentage
+FROM Meal
+JOIN Meal_Category ON Meal.item_id = Meal_Category.category_id
+JOIN Nutritional_Information ON Meal.item_id = Nutritional_Information.item_id
+JOIN Daily_Values_on_the_Nutrition ON Meal.item_id = Daily_Values_on_the_Nutrition.item_id
 ORDER BY Nutritional_Information.Calories;
 
