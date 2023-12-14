dcf527aff6e6f332752f4b8dd4f534295ce09dad
PeschykhinLev <91117670+PeschykhinLev@users.noreply.github.com>
2023-12-14 02:26:52 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,22 +1,20 @@
--- Значення кількості калорій у стравах з категорії "Сніданки"
-SELECT Calories FROM Nutritional_Information
-WHERE item_id = (
-    SELECT item_id FROM meal_category WHERE item_id =(
-		 SELECT category_id FROM category Where category_name = 'Breakfast'
-	)
-);
+-- Значення кількості протеїну у страві та її назва
+SELECT Meal.item_name, Nutritional_Information.protein
+FROM Meal
+JOIN Nutritional_Information ON Meal.item_id = Nutritional_Information.item_id
+ORDER BY protein
 
--- Категорія страви, які мають від 100 до 500 калорій
-SELECT Category.category_name
+-- Категорія страви, які мають від 100 калорій та кількість цих калорій
+SELECT Category.category_name, Nutritional_Information.Calories
+FROM Category
+JOIN Meal_Category ON Category.category_id = Meal_Category.category_id
+JOIN Nutritional_Information ON Meal_Category.item_id = Nutritional_Information.item_id;
+
+-- Назва страви, та впорядковане порівняння (за зростаннія) її кількості калорій та % від денної форми жирів
+SELECT Category.category_name, Nutritional_Information.Calories, Daily_Values_on_the_Nutrition.Total_Fat_Daily_Percentage
 FROM Category
 JOIN Meal_Category ON Category.category_id = Meal_Category.category_id
 JOIN Nutritional_Information ON Meal_Category.item_id = Nutritional_Information.item_id
-WHERE Nutritional_Information.Calories > 100 AND Nutritional_Information.Calories < 500;
-
-
--- Назва страви, яка має у собі більше 2% від денної норми споживання вітаміну С
-SELECT Meal.item_name
-FROM Meal
-JOIN Daily_Values_on_the_Nutrition ON Meal.item_id = Daily_Values_on_the_Nutrition.item_id
-WHERE Daily_Values_on_the_Nutrition.Vitamin_C_Daily_Percentage > 2;
+JOIN Daily_Values_on_the_Nutrition ON Meal_Category.item_id = Daily_Values_on_the_Nutrition.item_id
+ORDER BY Nutritional_Information.Calories;
 
